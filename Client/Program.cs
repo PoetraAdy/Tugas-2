eb9dab4d8059adea47afc8c1ad497b0a99536391
Poetra Ady <putraadi664@gmail.com>
2017-11-12 04:37:48 -0800

    Contoh Codingan TCP Send Time

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var broadcaster = new UdpClient();
+            var endPoint = new IPEndPoint(IPAddress.Broadcast, 4000);
+            var data = Encoding.ASCII.GetBytes("PING");
+            broadcaster.Send(data, data.Length, endPoint);
+            while(true)
+            {
+                var rcvByte = broadcaster.Receive(ref endPoint);
+                var rcvData = Encoding.ASCII.GetString(rcvByte, 0, rcvByte.Length);
+                Console.WriteLine("Data from server: {0}", rcvData);
+            }
+
+            broadcaster.Close();
+            Console.WriteLine(" ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TimerSend/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TimerSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int listenPort = 4000;
+            var serverAddr = new UdpClient(listenPort);
+            var endPoint = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    var rcvByte = serverAddr.Receive(ref endPoint);
+                    Console.WriteLine("Receive a broadcast from {0}", endPoint.ToString());
+                    var rcvData = Encoding.ASCII.GetString(rcvByte, 0, rcvByte.Length);
+                    Console.WriteLine("Data from broadcast: {0}", rcvData);
+
+                    while(true)
+                    {
+                        DateTime Waktuku = DateTime.Now;
+                        string Timeku = Waktuku.ToString("hh:mm:ss");
+                        var reply = Timeku;
+                        var replyByte = Encoding.ASCII.GetBytes(reply);
+                        serverAddr.Send(replyByte, replyByte.Length, endPoint);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+            Console.WriteLine(" ");
+            Console.ReadLine();
+        }
+    }
+}
